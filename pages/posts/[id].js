2bac74d02a7b2a7e7baea7e8572b2fc9d86ade7a
black-code1 <legrandmahamat1@gmail.com>
2021-07-09 16:50:55 +0100

    Done with dynamic routing

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,15 @@
 import Layout from '../../components/layout'
-import { getAllPostIds } from '../../lib/posts'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
@@ -9,8 +19,15 @@ export async function getStaticPaths() {
   }
 }
 
-export default function Post() {
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
+}
 
-  return <Layout>...</Layout>
-  
-};
